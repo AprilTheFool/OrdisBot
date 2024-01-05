8da46fb101e039f709ba62bb642de6359925dca9
April <122740391+AprilTheFool@users.noreply.github.com>
2024-01-06 04:20:02 +1000

    Improved dictionary formatting

--- index.js
+++ index.js
@@ -17,26 +17,33 @@ function getWarframeData() {
 }
 
 class Relic {
-    constructor(json) {
-      this.hard = json.Hard === true
-      this.modifier = {VoidT1:"<:lith:1192868176036364358>", VoidT2:"<:meso:1192868214758178936>", VoidT3:"<:neo:1192868235238985818>", VoidT4:"<:neo:1192868255786868798>", VoidT5:"<:requiem:1192868272094318693>"}[json.Modifier];
-      this.missionType = {
-        MT_SURVIVAL:"Survival",
-        MT_RESCUE:"Rescue",
-        MT_EXTERMINATION:"Extermination",
-        MT_INTEL:"Spy",
-        MT_DEFENSE:"Defense",
-        MT_CAPTURE:"Capture",
-        MT_TERRITORY:"Interception",
-        MT_ARTIFACT:"Disruption",
-        MT_MOBILE_DEFENSE:"Mobile Defense",
-        MT_SABOTAGE:"Sabotage",
-        MT_EXCAVATE:"Excavation",
-        MT_ASSAINATION:"Assasination",
-        MT_DISRUPTION:"Disruption"
-      }[json.MissionType];
-    }
+  constructor(json) {
+    this.hard = json.Hard === true
+    this.modifier = {VoidT1:"Lith", VoidT2:"Meso", VoidT3:"Neo", VoidT4:"Axi", VoidT5:"Requiem"}[json.Modifier];
+    this.modifierIcon ={
+      VoidT1:"<:lith:1192868176036364358>",
+      VoidT2:"<:meso:1192868214758178936>",
+      VoidT3:"<:neo:1192868235238985818>",
+      VoidT4:"<:neo:1192868255786868798>",
+      VoidT5:"<:requiem:1192868272094318693>"
+    }[json.Modifier];
+    this.missionType = {
+      MT_SURVIVAL:"Survival",
+      MT_RESCUE:"Rescue",
+      MT_EXTERMINATION:"Extermination",
+      MT_INTEL:"Spy",
+      MT_DEFENSE:"Defense",
+      MT_CAPTURE:"Capture",
+      MT_TERRITORY:"Interception",
+      MT_ARTIFACT:"Disruption",
+      MT_MOBILE_DEFENSE:"Mobile Defense",
+      MT_SABOTAGE:"Sabotage",
+      MT_EXCAVATE:"Excavation",
+      MT_ASSAINATION:"Assasination",
+      MT_DISRUPTION:"Disruption"
+    }[json.MissionType];
   }
+}
 
 // Grab warframe relic data every 1m
 let relicTimer = setInterval(() => {
@@ -48,7 +55,10 @@ let relicTimer = setInterval(() => {
           .then(json => {
             for (let i = 0; i < Object.keys(json.ActiveMissions).length; i++) {
                 let relicObj = new Relic(json.ActiveMissions[i]);
-              relicMessage = relicMessage + `${relicObj.modifier} -*** ${relicObj.missionType}***${relicObj.hard === true ? " <:steelpath:1192865952912646297>" : ""}\n`
+              relicMessage = relicMessage + `${relicObj.modifierIcon} -*** ${relicObj.missionType}***${relicObj.hard === true ? " <:steelpath:1192865952912646297>" : ""}\n`
+              if (relicObj.missionType == undefined){
+                console.log(json.ActiveMission[i].MissionType);
+              }
             }
             channel.bulkDelete(5);
             channel.send(relicMessage);
